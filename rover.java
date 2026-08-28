c76a360864aac1633aab6c017168701139944efb
maycongomes151103-netizen <maycongomes151103@gmail.com>
2026-08-28 20:59:35 -0300

    coloquei um codigo simples.

---
+++ rover.java
@@ -0,0 +1,9 @@
+public class Soma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int resultado = a + b;
+
+        System.out.println("A soma de " + a + " + " + b + " é: " + resultado);
+    }
+}
